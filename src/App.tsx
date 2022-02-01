5ccb9e55b468bfa94ca4dccb811587c9f588b798
Andrii Rudkovskyi <arudkovskyy@Andriys-MacBook-Pro.local>
2022-02-01 16:15:27 +0200

    add local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
 import TodoList from './components/TodoList';
 import { Todo } from './model';
 
+const getInitialTodoState = () => {
+  const savedTodos = localStorage.getItem('todosLS');
+  return savedTodos ? JSON.parse(savedTodos) : [];
+}
+
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>('');
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, setTodos] = useState<Todo[]>(getInitialTodoState);
 
+  useEffect(() => {
+    localStorage.setItem('todosLS', JSON.stringify(todos));
+  }, [todos]);
+  
   const handleAdd = useCallback((e: React.FormEvent) => {
     e.preventDefault();
     if (todo) {
-      setTodos([...todos, { id: Date.now(), todo, isDone: false }]); setTodo('');
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo('');
     }
   }, [todo, todos]);
 
